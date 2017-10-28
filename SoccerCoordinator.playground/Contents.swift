c42f6425b77a1df11e96af323e71a7d963bd3ffc
Nate <nate@fifty2digital.com>
2017-10-27 20:37:32 -0400

    test

---
+++ SoccerCoordinator.playground/Contents.swift
@@ -0,0 +1,9 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+// this is just a test by Nate
+
+
---
+++ SoccerCoordinator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SoccerCoordinator
+//
+//  Created by Nate Frisella on 10/27/17.
+//  Copyright © 2017 Nate Frisella. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
